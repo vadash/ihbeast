dacb979e40178e2a85a34709bbb0de2d0ae91ec6
vadash <vadash@gmail.com>
2019-08-17 12:29:46 +0300

    dont look at bottom (mmr too low), only 1st 2nd places

--- ihbeast.lua
+++ ihbeast.lua
@@ -25,15 +25,22 @@ stateMachine:register("arena_top", "choose_rival", function ()
 end)
 
 stateMachine:register("choose_rival", "heroes_formation", function ()
-    stateMachine:log("not implemented #1")
     local found = false
-    if not found then found = stateMachine:find("btn_cankill_1_.png") end -- vn rex dancing
-    if not found then found = stateMachine:find("btn_cankill_2_.png") end -- hut can
-    if not found then found = stateMachine:find("btn_cankill_3_.png") end -- mad 1r
-    if found then
-        click(found:offset(923 - 351, 0))
+    local n = 0
+    while not found and n < 50 do
+        n = n + 1
+        if math.fmod(n, 5) == 0 then wait(60) end -- 1 minute pause every 5 searches
+        if math.fmod(n, 13) == 0 then wait(10 * 60) end -- 10 minute pause every 13 searches
+        if math.fmod(n, 29) == 0 then wait(30 * 60) end -- 30 minute pause every 29 searches
+        if not found then found = stateMachine:find("btn_cankill_1_.png") end -- vn rex dancing
+        if not found then found = stateMachine:find("btn_cankill_2_.png") end -- hut can
+        if not found then found = stateMachine:find("btn_cankill_3_.png") end -- mad 1r
+        if found then
+            click(found:offset(923 - 351, 0)) -- attack
+        else
+            stateMachine:click("btn_choose_rival_.png") -- refresh
+        end
     end
-    if not found then stateMachine:click("btn_choose_rival_.png") end -- refresh
     return stateMachine:waitKnownState(10)
 end)
 
